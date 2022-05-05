4d1436e050fd6207847546b218d9564df37582c2
KetchuppOfficial <dolgov04@list.ru>
2022-05-05 15:19:42 +0300

    Mistake in the name of <Draw_Optimized> corrected

--- Alpha_Blending.hpp
+++ Alpha_Blending.hpp
@@ -7,7 +7,7 @@
 #define VERT_SIZE 600
 #define HOR_SIZE  800
 
-void Draw (const char *front_name, const char *back_name);
+void Draw_Optimized (const char *front_name, const char *back_name);
 void Draw_Unoptimized (const char *front_name, const char *back_name);
 
 #endif
