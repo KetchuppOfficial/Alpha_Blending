0e35d4a5a1ed5f738be4550a9348194de0ad257f
KetchuppOfficial <dolgov04@list.ru>
2022-05-07 22:41:00 +0300

    Changes in structure of the project

---
+++ include/Alpha_Blending.hpp
@@ -0,0 +1,13 @@
+#ifndef ALPHA_BLENDING_INCLUDED
+#define ALPHA_BLENDING_INCLUDED
+
+#include <emmintrin.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MEASURE 1
+#define N_FRAMES 1000
+
+void Draw (const char *front_name, const char *back_name);
+
+#endif
